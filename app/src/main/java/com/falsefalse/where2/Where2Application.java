2a95012494fa94bf61c7a5187fc4b43861ccc8ce
Bartul Gašperov <bartul.gaperov@gmail.com>
2022-11-14 20:59:24 +0100

    Initialized db and seeded it with few starting values

--- app/src/main/java/com/falsefalse/where2/Where2Application.java
+++ app/src/main/java/com/falsefalse/where2/Where2Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Where2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Where2Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Where2Application.class, args);
+    }
 }
